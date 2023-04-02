382788bf39708d4cf3ab20a1e4a98389e2127ebf
Alex <piletskui21@gmail.com>
2023-04-02 22:15:31 +0300

    Better

--- server.py
+++ server.py
@@ -4,8 +4,13 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 sock.bind(('', 13573))
+
 print ('[+] Ожидания соединения')
+
 sock.listen(0)
+
 conn, addr = sock.accept()
+
 print ('[+] Соединение установлено!:', addr)
+
 conn.close()
