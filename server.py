b3b1bdb5ea2368040ff66ef0d69ae444f2323aa0
Alex <piletskui21@gmail.com>
2023-04-08 10:20:45 +0300

    better1

--- server.py
+++ server.py
@@ -3,6 +3,12 @@ print("[!] Powerd by Error")
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+#коментарий
+
+#ещё один коментарий
+
+#новый коммит
+
 sock.bind(('', 13573))
 print ('[+] Ожидания соединения')
 sock.listen(0)
